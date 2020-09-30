bdab94b83d6550d3f348c2527789d4adfde2b98c
ElenaNam <zaidelsonelena@gmail.com>
2020-09-30 17:18:35 +0400

    init: start and finish task Typical Arrays Problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
 exports.min = function min (array) {
+  if (array && array.length)  {
+    return Math.min.apply( Math, array );
+  }
   return 0;
 }
 
 exports.max = function max (array) {
+  if (array && array.length)  {
+    return Math.max.apply( Math, array );
+  }  
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if (array && array.length)  {
+    return array.reduce((a, b) => (a + b)) / array.length;
+    
+  }  
   return 0;
 }
